83b0f539ea2393b7f7f40bc288b61c24809e8a09
asbdu023221822 <144670834+asbdu023221822@users.noreply.github.com>
2023-09-20 12:32:54 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,5 +1,27 @@
-year=int(input("Enter a year:"))
-if(year % 4==0):
-  print("The given year is leap year")
-else:
-  print("The given year is not a leap year")
+class player:
+
+  def play(self):
+    print("the player is playing cricket:")
+
+
+#Define the drived class Bastman
+class Batsman(player):
+
+  def play(self):
+    print("the batsman is batting. ")
+
+
+#Define the driver class Bowler
+class Bowler:
+
+  def play(self):
+    print("the bowler is Bowling")
+
+
+#create object of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+#call the play() method for each object
+batsman.play()
+bowler.play()
